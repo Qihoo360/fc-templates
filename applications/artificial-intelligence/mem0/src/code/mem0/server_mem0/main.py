87d6200d039739d9dd8745dbc2b6c864bfd24717
zhangyang10 <zhangyang10@360.cn>
2025-08-19 11:11:52 +0800

    fix: mem0 route add v1

--- applications/artificial-intelligence/mem0/src/code/mem0/server_mem0/main.py
+++ applications/artificial-intelligence/mem0/src/code/mem0/server_mem0/main.py
@@ -95,6 +95,7 @@ app = FastAPI(
     title="Mem0 REST APIs",
     description="A REST API for managing and searching memories for your AI Agents and Apps.",
     version="1.0.0",
+    root_path="/v1",
 )
 
 
